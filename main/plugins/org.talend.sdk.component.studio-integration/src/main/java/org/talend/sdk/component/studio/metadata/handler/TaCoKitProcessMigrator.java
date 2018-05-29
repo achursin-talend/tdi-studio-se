2859e344c2c0b2e232c1abf93db5bfddd2f1ca9a
Ivan Honchar <ivan.honchar@globallogic.com>
2018-05-29 22:41:02 +0300

    feat(TCOMP-816): implement Tacokit process migration during import

---
+++ main/plugins/org.talend.sdk.component.studio-integration/src/main/java/org/talend/sdk/component/studio/metadata/handler/TaCoKitProcessMigrator.java
@@ -0,0 +1,136 @@
+package org.talend.sdk.component.studio.metadata.handler;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.common.util.EList;
+import org.talend.commons.exception.ExceptionHandler;
+import org.talend.commons.exception.PersistenceException;
+import org.talend.core.model.properties.ConnectionItem;
+import org.talend.core.model.properties.Item;
+import org.talend.core.model.properties.ProcessItem;
+import org.talend.core.model.properties.Property;
+import org.talend.core.model.properties.impl.ProcessItemImpl;
+import org.talend.core.model.repository.IRepositoryViewObject;
+import org.talend.core.repository.model.ProxyRepositoryFactory;
+import org.talend.designer.core.model.utils.emf.talendfile.impl.NodeTypeImpl;
+import org.talend.designer.core.model.utils.emf.talendfile.impl.ProcessTypeImpl;
+import org.talend.repository.ProjectManager;
+import org.talend.repository.items.importexport.handlers.imports.IImportResourcesHandler;
+import org.talend.repository.items.importexport.handlers.imports.ImportCacheHelper;
+import org.talend.repository.items.importexport.handlers.model.ImportItem;
+import org.talend.repository.items.importexport.manager.ResourcesManager;
+import org.talend.repository.model.IProxyRepositoryFactory;
+import org.talend.sdk.component.studio.Lookups;
+import org.talend.sdk.component.studio.websocket.WebSocketClient.V1Component;
+import org.talend.sdk.studio.process.TaCoKitNode;
+
+public class TaCoKitProcessMigrator implements IImportResourcesHandler {
+    
+    private final V1Component client = Lookups.client().v1().component(); 
+
+    @Override
+    public void prePopulate(IProgressMonitor monitor, ResourcesManager resManager) {
+        // no-op
+    }
+
+    @Override
+    public void postPopulate(IProgressMonitor monitor, ResourcesManager resManager, ImportItem[] populatedItemRecords) {
+        // no-op
+    }
+
+    @Override
+    public void preImport(IProgressMonitor monitor, ResourcesManager resManager, ImportItem[] checkedItemRecords,
+            ImportItem[] allImportItemRecords) {
+        // no-op
+    }
+
+    @Override
+    public void postImport(IProgressMonitor monitor, ResourcesManager resManager, ImportItem[] importedItemRecords) {
+        if (importedItemRecords == null) {
+            return;
+        }
+        for (final ImportItem importItem : importedItemRecords) {
+            try {
+                getItem(importItem).ifPresent(processItem -> {
+                    migrateItem(processItem);
+                });
+            } catch (Exception e) {
+                ExceptionHandler.process(e);
+            }
+        }
+    }
+    
+    /**
+     * Loads repository item. Implementation was copied from
+     * ImportBasicHandler.applyMigrationTasks()
+     * 
+     * @param importItem
+     *            item which is imported
+     * @return Process item stored in repository
+     * @throws PersistenceException
+     */
+    private Optional<ProcessItem> getItem(final ImportItem importItem) throws PersistenceException {
+        List<IRepositoryViewObject> allVersion = ProxyRepositoryFactory.getInstance().getAllVersion(
+                ProjectManager.getInstance().getCurrentProject(), importItem.getItemId(), importItem.getImportPath(),
+                importItem.getRepositoryType());
+        for (IRepositoryViewObject repositoryObject : allVersion) {
+            if (repositoryObject.getProperty().getVersion().equals(importItem.getItemVersion())) {
+                final Item item = repositoryObject.getProperty().getItem();
+                if (ProcessItem.class.isInstance(item)) {
+                    return Optional.of((ProcessItem) item);
+                }
+            }
+        }
+        return Optional.empty();
+    }
+    
+    private void migrateItem(final ProcessItem item) {
+//        if (ProcessItemImpl.class.isInstance(item)) {
+//            ProcessItemImpl processItem = (ProcessItemImpl) item;
+            final ProcessTypeImpl processType = (ProcessTypeImpl) item.getProcess();
+            migrateProcess(processType);
+            save(item);
+//        } // else no-op
+    }
+    
+    private void migrateProcess(final ProcessTypeImpl process) {
+        migrateNodes(process.getNode());
+    }
+    
+    @SuppressWarnings("rawtypes")
+    private void migrateNodes(final EList nodes) {
+        for (final Object elem : nodes) {
+            NodeTypeImpl node = (NodeTypeImpl) elem;
+            if (TaCoKitNode.isTacokit(node)) {
+                final TaCoKitNode tacokitNode = new TaCoKitNode(node);
+                if (tacokitNode.needsMigration()) {
+                    migrateNode(tacokitNode);
+                }
+            }
+        }
+    }
+    
+    private void migrateNode(final TaCoKitNode node) {
+        final Map<String, String> migratedProperties = client.migrate(node.getId(), node.getPersistedVersion(),
+                node.getProperties());
+        node.migrate(migratedProperties);
+    }
+    
+    private void save(final Item item) {
+        final IProxyRepositoryFactory factory = ProxyRepositoryFactory.getInstance();
+        try {
+            factory.save(item);
+        } catch (PersistenceException e) {
+            logError(e);
+        }
+    }
+    
+    protected void logError(Exception e) {
+        ImportCacheHelper.getInstance().setImportingError(true);
+        ExceptionHandler.process(e);
+    }
+
+}
---
+++ main/plugins/org.talend.sdk.component.studio-integration/src/main/java/org/talend/sdk/studio/process/TaCoKitNode.java
@@ -0,0 +1,165 @@
+package org.talend.sdk.studio.process;
+
+import static org.talend.core.model.process.EParameterFieldType.TEXT;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+import org.eclipse.emf.common.util.EList;
+import org.talend.designer.core.model.components.EParameterName;
+import org.talend.designer.core.model.utils.emf.talendfile.impl.ElementParameterTypeImpl;
+import org.talend.designer.core.model.utils.emf.talendfile.impl.NodeTypeImpl;
+import org.talend.sdk.component.server.front.model.ComponentDetail;
+import org.talend.sdk.component.server.front.model.SimplePropertyDefinition;
+import org.talend.sdk.component.studio.Lookups;
+import org.talend.sdk.component.studio.model.parameter.WidgetTypeMapper;
+
+/**
+ * Wrapper for NodeTypeImpl - class which represents persisted node (component in the process).
+ * Provides additional functionality
+ */
+public final class TaCoKitNode {
+    
+    private final NodeTypeImpl node;
+    
+    private final ComponentDetail detail;
+    
+    private static final Set<String> TECHNICAL_PARAMETERS = new HashSet(
+            Arrays.asList(EParameterName.UNIQUE_NAME.getName())); 
+    
+    public TaCoKitNode(final NodeTypeImpl node) {
+        Objects.requireNonNull(node, "Node should not be null");
+        if(!isTacokit(node)) {
+            throw new IllegalArgumentException("It is not Tacokit node " + node.getComponentName());
+        }
+        this.node = node;
+        this.detail = Lookups.service().getDetail(node.getComponentName()).orElseThrow(() -> 
+            new IllegalStateException("No component detail for " + node.getComponentName()));
+    }
+    
+    public String getId() {
+        return detail.getId().getId();
+    }
+    
+    public boolean needsMigration() {
+        final int currentVersion = getCurrentVersion();
+        final int persistedVersion = getPersistedVersion();
+        if (currentVersion < persistedVersion) {
+            throw new IllegalStateException(
+                    "current version: " + currentVersion + " persisted version: " + persistedVersion);
+        }
+        return currentVersion != persistedVersion;
+    }
+    
+    public Map<String, String> getProperties() {
+        Map<String, String> properties = new HashMap<>();
+        EList parameters = node.getElementParameter();
+        for (final Object elem : parameters) {
+            ElementParameterTypeImpl parameter = (ElementParameterTypeImpl) elem;
+            if (!TECHNICAL_PARAMETERS.contains(parameter.getName())) {
+                properties.put(parameter.getName(), parameter.getValue());
+            }
+        }
+        return properties;
+    }
+    
+    @SuppressWarnings("unchecked")
+    public void migrate(final Map<String, String> properties) {
+        final Map<String, String> technicalProperties = getTechnicalProperties();
+        node.getElementParameter().clear();
+        technicalProperties.forEach((name, value) -> {
+            final ElementParameterTypeImpl parameter = createTechnicalParameter(name, value);
+            node.getElementParameter().add(parameter);
+        });
+        properties.forEach((name, value) -> {
+            if (isComponentProperty(name)) {
+                final ElementParameterTypeImpl parameter = createParameter(name, value);
+                node.getElementParameter().add(parameter);
+            }
+        });
+    }
+    
+    /**
+     * Workaround method to filter *validation properties, which should not be serialized normally.
+     * This method should be removed after skipping *validation properties serialization.
+     * 
+     * @return true, if property is stored in SimplePropertyDefinition
+     */
+    private boolean isComponentProperty(final String name) {
+        return detail.getProperties().stream().filter(property -> property.getPath().equals(name)).count() != 0;
+    }
+    
+    private ElementParameterTypeImpl createParameter(final String name, final String value) {
+        final ElementParameterTypeImpl parameter = new ElementParameter();
+        parameter.setName(name);
+        parameter.setValue(value);
+        parameter.setField(getPropertyType(name));
+        return parameter;
+    }
+    
+    private ElementParameterTypeImpl createTechnicalParameter(final String name, final String value) {
+        final ElementParameterTypeImpl parameter = new ElementParameter();
+        parameter.setName(name);
+        parameter.setValue(value);
+        parameter.setField(TEXT.getName());
+        return parameter;
+    }
+    
+    private SimplePropertyDefinition getProperty(final String name) {
+        return detail.getProperties().stream().filter(property -> property.getPath().equals(name)).findFirst().orElseThrow(
+                () -> new IllegalArgumentException("Can't find property for name: " + name));
+    }
+    
+    private String getPropertyType(final String name) {
+        final SimplePropertyDefinition property = getProperty(name);
+        final String type = new WidgetTypeMapper().getFieldType(property).getName();
+        return type;
+    }
+    
+    /**
+     * Returns technical node properties. Such properties should not be lost during migration
+     * 
+     * @return a map with technical properties 
+     */
+    private Map<String, String> getTechnicalProperties() {
+        Map<String, String> properties = new HashMap<>();
+        EList parameters = node.getElementParameter();
+        for (final Object elem : parameters) {
+            ElementParameterTypeImpl parameter = (ElementParameterTypeImpl) elem;
+            if (TECHNICAL_PARAMETERS.contains(parameter.getName())) {
+                properties.put(parameter.getName(), parameter.getValue());
+            }
+        }
+        return properties;
+    }
+    
+    public int getCurrentVersion() {
+        return detail.getVersion();
+    }
+    
+    public int getPersistedVersion() {
+        return Integer.parseInt(node.getComponentVersion());
+    }
+    
+    public static boolean isTacokit(final NodeTypeImpl node) {
+        return Lookups.service().getDetail(node.getComponentName()).isPresent();
+    }
+    
+    /**
+     * This class extends ElementParameterTypeImpl to allow instance creation
+     */
+    private static class ElementParameter extends ElementParameterTypeImpl {
+        
+        /**
+         * Extends super class constructor visibility
+         */
+        public ElementParameter() {
+            super();
+        }
+    }
+
+}
